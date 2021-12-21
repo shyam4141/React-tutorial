79d9693685d6a553efd48aecb315e69e0f22fce7
anilxhrestha@gmail.com <anilxhrestha@gmail.com>
2021-12-21 14:13:31 -0600

    Assignment Done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,34 @@
-import React from "react";
 
-function Home() {
-  return (
-    <div>
-      <p>Hello everyone!!</p>
-    </div>
-  );
+import React, { Component } from 'react'
+
+
+function handleReadMore()
+{
+    alert('You clicked Readmore');
 }
 
-export default Home;
+// Component named Home is Created and exported 
+export default class Home extends Component {
+
+  courseTraining = "React & Redux";
+
+  render() {
+    const name = "Anil"
+    var lastName = "Shrestha"
+    return (
+      <div className = "componentClass">
+          <div className="content">
+              {/* use of localVariable */}
+              <h1>Hi This is {name} {lastName}</h1>
+              {/* use of Global Variable  */}
+              <h2>I am enrolled in {this.courseTraining}</h2> 
+
+              <button className = "btn" onClick = {handleReadMore}>Read More</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const test = new Home();
+console.log(test);
